f76e6b3cd5e265e87f9c0c0b471bfe171ff89f22
js <joshtwoone@gmail.com>
2019-03-22 19:36:02 -0700

    successfully pulling data from backend to news view, need to decide if it will be processed in the view with php or if i should move api request to javascript client code

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -2,13 +2,12 @@
 class News extends CI_Controller {
     public function __construct(){
         parent::__construct();
-        // $this->load->model('news_model');
+        $this->load->model('news_model');
         $this->load->helper('url_helper');
     }
 
     public function index() {
-        $data['news'] = "mock data news"; // $this->news_model->get_news();
-
+        $data["news"] = $this->news_model->get_news();
         $this->load->view('news', $data);
     }
 }
---
+++ application/models/News_model.php
@@ -0,0 +1,22 @@
+<?php
+class News_model extends CI_Model {
+
+        public function __construct()
+        {
+                // $this->load->database();
+        }
+
+        public function get_news($slug = FALSE)
+        {
+            //get articles from newsAPI.org
+            return $this->get_url();
+            
+        }
+
+        private function get_url($url = FALSE) { // API KEY: 597116e2bbea465fa794a86c3d6708bc
+            $response = file_get_contents('https://newsapi.org/v2/top-headlines?country=co&apiKey=597116e2bbea465fa794a86c3d6708bc'); // country=co (Colombia)
+            log_message('debug', $response);
+            return $response;
+        }
+
+    }
