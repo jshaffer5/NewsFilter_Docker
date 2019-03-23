7c981a3748374f2f7d521f916d825fc1bf44fcd8
js <joshtwoone@gmail.com>
2019-03-22 20:12:44 -0700

    parsed json and articles are now sent as an array to the news view

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -15,8 +15,16 @@ class News_model extends CI_Model {
 
         private function get_url($url = FALSE) { // API KEY: 597116e2bbea465fa794a86c3d6708bc
             $response = file_get_contents('https://newsapi.org/v2/top-headlines?country=co&apiKey=597116e2bbea465fa794a86c3d6708bc'); // country=co (Colombia)
-            log_message('debug', $response);
-            return $response;
+            // log_message('debug', $response);
+            $articles = $this->format_response($response);
+            return $articles;
+        }
+
+        private function format_response($response) {
+            $obj = json_decode($response, true);
+            $articles = $obj["articles"];
+            log_message('debug', $articles[0]["title"]);
+            return $articles;
         }
 
     }
