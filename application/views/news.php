07db1674b4568e88dde01b16f14007ffc72eba80
js <joshtwoone@gmail.com>
2019-03-24 00:11:13 -0700

    added images for articles

--- application/views/news.php
+++ application/views/news.php
@@ -2,6 +2,8 @@
 <html>
 <head>
     <title>News List</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
     
 </head>
@@ -9,23 +11,34 @@
 <div class="container">
     <h1>News List</h1>
     
-    <div id="list-flex">
+    <div id="list-flex" style="display:flex">
         <div id="list" class="news-list">
-            <ul id="list-articles">
+            <!-- <div class="article-card" style="display:flex"> -->
                 <!-- <li v-for="article in articlesList">{{ article.headline }} - {{ article.content }}</li> -->
+
                 <?php foreach ($articles as $article): ?>
-                    <strong><?php echo $article['title']; ?></strong><a id="read-more" href="<?php echo $article['url'] ?>" target="_blank"> Read More</a>
-                    <p><?php echo $article['description']; ?></p>
+
+                    <div class="card-text">
+
+                        <strong> 
+
+                            <img class="card-img" src="<?php echo $article['urlToImage'] ?>" alt="no image"> 
+                            <?php echo $article['title']; ?>
+
+                        </strong> <a id="read-more" href="<?php echo $article['url'] ?>" target="_blank"> Read More</a>
+
+                        <p><?php echo $article['description']; ?></p>
+
+                    </div>
 
                     <hr>
                 <?php endforeach; ?>
-            </ul>
+            <!-- </div> -->
+        
         </div>
     </div>
 </div>
-<!--DEVELOPMENT ONLY LATEST RELEASE -->
-<!-- <script src="https://cdn.jsdelivr.net/npm/vue"></script> -->
-<!-- PRODUCTION USE STABLE RELEASE -->
+<!-- PRODUCTION USE STABLE VUE.JS RELEASE -->
 <script src="https://cdn.jsdelivr.net/npm/vue@2.6.8/dist/vue.js"></script>
 <script src="javascript/news.js"></script>
 </body>
