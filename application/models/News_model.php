506089ff96c386249b17a29daf52f00bda1c4e2d
js <js@js-Mac-mini.local>
2025-08-27 16:29:15 -0700

    fixed file_get_contents http request by adding custom User-Agent header to request. This makes request appear more like it came from a browser

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -14,7 +14,14 @@ class News_model extends CI_Model {
         }
 
         private function get_url($url = FALSE) { // API KEY: 597116e2bbea465fa794a86c3d6708bc
-            $response = file_get_contents('https://newsapi.org/v2/top-headlines?country=co&apiKey=597116e2bbea465fa794a86c3d6708bc'); // country=co (Colombia)
+            $context = stream_context_create([
+                'http' => [
+                    'header' => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36\r\n"
+                ]
+            ]);
+            $response = file_get_contents('http://newsapi.org/v2/top-headlines?country=us&apiKey=597116e2bbea465fa794a86c3d6708bc', false, $context);
+
+            
             // log_message('debug', $response);
             $articles = $this->format_response($response);
             return $articles;
